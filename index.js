d96f85af347fd958d8f9cfe5e1bde83e3a6e0044
Marion Bustamante <melchisedec.bustamante@gmail.com>
2023-12-03 17:56:08 -0600

    Cors actualizados

--- index.js
+++ index.js
@@ -9,27 +9,27 @@ const app = express();
 
 //Conectamos a la DB
 
-app.use(cors({origin: 'http://localhost:4200'}));
+app.use(cors());
 conectarDB()
 app.set('trust proxy', 1);
 
 
 //Middleware
 //Se usa para ejecutar funciones middleware
-app.use(bodyParser.json({ limit: '200mb' }));
+app.use(bodyParser.json({limit: '200mb'}));
 app.use(bodyParser.urlencoded({
-    limit: '200mb',
-    extended: true
-}
+        limit: '200mb',
+        extended: true
+    }
 ));
 
 //rutas
 
-app.use('/api/usuarios',require('./routes/usuario'));
-app.use('/api/productos',require('./routes/producto'));
-app.use('/empresa/',require('./routes/empresa'));
-app.use('/admin/',require('./routes/admin'));
-app.use('/api/paginas',require('./routes/pagina'));
+app.use('/api/usuarios', require('./routes/usuario'));
+app.use('/api/productos', require('./routes/producto'));
+app.use('/empresa/', require('./routes/empresa'));
+app.use('/admin/', require('./routes/admin'));
+app.use('/api/paginas', require('./routes/pagina'));
 
 
 app.listen(3000, () => {
